867659b6211ae8d9535c44e7cf99d85f734d3cac
AditiaGA <hss69888@gmail.com>
2024-08-29 21:07:44 +0700

    handson 1

---
+++ BuatVariable.java
@@ -0,0 +1,13 @@
+public class BuatVariable {
+    public static void main(String[] args) {
+        String name; //deklarasi
+        name = "Hizkia Ardiyanto";
+
+        String showInfo = "Nama saya" + name;
+
+        String universitasAdventIndonesia;
+        int Hizkia = 10;
+        final float nilaiPhytagoras = 3.14F;
+
+    }
+}
---
+++ TipeDataString.java
@@ -0,0 +1,12 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        //Perbedaan primitive dan non primitive
+
+        String firstName = "Hizkia";
+        String lastName = "Ardiyanto";
+
+        //Concat
+        String fullName = firstName.concat ( " "). concat ( lastName );
+        System.out.println(fullName);
+    }
+}
